06e888fc26444c9ed3fdc8cac6400334c039f7d0
sujit <sujitbokade2014@gmail.com>
2022-07-26 17:25:34 +0530

    Initial Commit

---
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedList <T>{
+
+
+void print(T var){
+    System.out.print(var+"->");
+}
+
+}
---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList1=new LinkedList<>();
+          linkedList1.print(56);
+          linkedList1.print(30);
+          linkedList1.print(70);
+    }
+}
