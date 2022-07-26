ccd5bfd764144cbe1c6e48ccf90f425f5e7a4c36
sujit <sujitbokade2014@gmail.com>
2022-07-26 17:29:53 +0530

    [Sujit]Simple LinkedList

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,10 +1,8 @@
 package com.bridgelabz.linkedlist;
 
 public class LinkedList <T>{
-
-
-void print(T var){
+    void print(T var){
     System.out.print(var+"->");
-}
+   }
 
 }
--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -3,8 +3,8 @@ package com.bridgelabz.linkedlist;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList1=new LinkedList<>();
-          linkedList1.print(56);
-          linkedList1.print(30);
-          linkedList1.print(70);
+           linkedList1.print(56);
+           linkedList1.print(30);
+           linkedList1.print(70);
     }
 }
