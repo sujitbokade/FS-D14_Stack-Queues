63905457493363425dd1f564f542464e038be1e6
sujit <sujitbokade2014@gmail.com>
2022-07-28 22:30:22 +0530

    [Sujit] Create Queues

---
+++ src/com/bridgelabz/stack/Queue.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.stack;
+
+import com.bridgelabz.linkedlist.LinkedList;
+
+public class Queue<T> {
+    LinkedList<T> linkedList;
+
+    public Queue() {
+        this.linkedList = new LinkedList<>();
+    }
+
+    public void enqueu(T key) {
+        linkedList.add(key);
+    }
+    public void display() {
+        linkedList.print();
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueu(56);
+        queue.enqueu(30);
+        queue.enqueu(70);
+        queue.display();
+    }
+}
+
